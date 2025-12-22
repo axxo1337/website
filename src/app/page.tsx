2b3d8c42f1f60a7b3ff7a8e872cb6f26f1514758
axxo1337 <58981352+axxo1337@users.noreply.github.com>
2025-12-22 16:44:24 -0500

    feat: Formatted data for future use with DB

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,63 @@ import Work from "@/components/page/Home/Work";
 import { MoveRight } from "lucide-react";
 import Link from "next/link";
 
+//
+// [SECTION] Defines
+//
+
+const socials = [
+  {
+    href: "https://www.youtube.com/@axxo1337",
+    title: "YouTube",
+    iconPath: "/images/socials/youtube.png",
+  },
+  {
+    href: "https://www.instagram.com/axxo1337",
+    title: "Instagram",
+    iconPath: "/images/socials/instagram.png",
+  },
+  {
+    href: "https://www.reddit.com/r/aXXo/",
+    title: "Reddit",
+    iconPath: "/images/socials/reddit.png",
+  },
+  {
+    href: "https://discord.gg/tdEAJU2XwZ",
+    title: "Discord (apply to join)",
+    iconPath: "/images/socials/discord.png",
+  },
+  {
+    href: "https://github.com/axxo1337",
+    title: "Github",
+    iconPath: "/images/socials/github.png",
+  },
+];
+
+const bestCreations: TWork[] = [
+  {
+    title: "Virtual Memory Explained",
+    description:
+      "What are memory pages? What exactly is an address space? Well that's what you'll learn today!",
+    thumbnailPath: "/images/thumbnails/virtual-memory-explained.png",
+    createdAt: new Date(2025, 4, 19),
+    type: "video",
+    href: "/video/virtual-memory-explained",
+  },
+  {
+    title: "AntiDebug",
+    description:
+      "A simple terminal interface tool to test Windows x86_64 anti-debugging techniques.",
+    thumbnailPath: "/images/thumbnails/antidebug.png",
+    createdAt: new Date(2025, 7, 10),
+    type: "tool",
+    href: "/project/antidebug",
+  },
+];
+
+//
+// [SECTION] Content
+//
+
 export default function Home() {
   return (
     <Main
@@ -41,22 +98,9 @@ export default function Home() {
       </Section>
       <Section subtitle="What do I make?" title="My best work">
         <div className="grid md:grid-cols-2 gap-6 md:gap-8">
-          <Work
-            title="Virtual Memory Explained"
-            description="What are memory pages? What exactly is an address space? Well that's what you'll learn today!"
-            thumbnailPath="/images/thumbnails/virtual-memory-explained.png"
-            createdAt={new Date(2025, 4, 19)}
-            type="video"
-            href="/video/virtual-memory-explained"
-          />
-          <Work
-            title="AntiDebug"
-            description="A simple terminal interface tool to test Windows x86_64 anti-debugging techniques."
-            thumbnailPath="/images/thumbnails/antidebug.png"
-            createdAt={new Date(2025, 7, 10)}
-            type="tool"
-            href="/project/antidebug"
-          />
+          {bestCreations.map((bestCreation, bestCreationIndex) => (
+            <Work key={`bestCreation-${bestCreationIndex}`} {...bestCreation} />
+          ))}
         </div>
         <hr className="my-4 md:my-6 border-white/20" />
         <div className="text-xl md:text-2xl flex items-center justify-between">
@@ -75,33 +119,24 @@ export default function Home() {
       </Section>
       <Section subtitle="How to reach me?" title="My social media">
         <ul className="flex flex-wrap gap-6 gap-y-4">
-          <Social
-            href="https://www.youtube.com/@axxo1337"
-            title="YouTube"
-            iconPath="/images/socials/youtube.png"
-          />
-          <Social
-            href="https://www.instagram.com/axxo1337"
-            title="Instagram"
-            iconPath="/images/socials/instagram.png"
-          />
-          <Social
-            href="https://www.reddit.com/r/aXXo/"
-            title="Reddit"
-            iconPath="/images/socials/reddit.png"
-          />
-          <Social
-            href="https://discord.gg/tdEAJU2XwZ"
-            title="Discord (apply to join)"
-            iconPath="/images/socials/discord.png"
-          />
-          <Social
-            href="https://github.com/axxo1337"
-            title="Github"
-            iconPath="/images/socials/github.png"
-          />
+          {socials.map((social, socialIndex) => (
+            <Social key={`social-${socialIndex}`} {...social} />
+          ))}
         </ul>
       </Section>
     </Main>
   );
 }
+
+//
+// [SECTION] Types
+//
+
+type TWork = {
+  title: string;
+  description: string;
+  type: "video" | "tool" | "library";
+  href: string;
+  createdAt: Date;
+  thumbnailPath: string;
+};
