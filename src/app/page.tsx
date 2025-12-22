97c27cef5fa7496e51db88a61af52dd7387804dd
axxo1337 <58981352+axxo1337@users.noreply.github.com>
2025-12-22 13:05:09 -0500

    feat: Added h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
-    <main>
-      
+    <main className="mt-12">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-6xl font-semibold">About me</h1>
+        <div className="flex gap-5 items-center font-medium text-white/70">
+          <span>Created • 12/21/2025</span>
+          <span>Last updated • 12/22/2025</span>
+        </div>
+      </div>
     </main>
   );
 }
