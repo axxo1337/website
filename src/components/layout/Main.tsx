c3778af8a2b03efe7fba6c5a1612dfd974d814a2
axxo1337 <58981352+axxo1337@users.noreply.github.com>
2025-12-24 12:58:17 -0500

    Update Main.tsx

--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -6,8 +6,18 @@ export default function Main({ title, createdAt, updatedAt, children }: Main) {
       <div className="flex flex-col gap-2">
         <h1 className="text-4xl md:text-6xl font-semibold">{title}</h1>
         <div className="text-sm md:text-base flex flex-wrap gap-5 gap-y-1 items-center font-medium text-white/75 mt-1">
-          <span>Created • {format(createdAt, "MM/dd/yyyy")}</span>
-          <span>Last updated • {format(updatedAt, "MM/dd/yyyy")}</span>
+          <span>
+            Created •{" "}
+            <time dateTime={createdAt.toISOString()}>
+              {format(createdAt, "MM/dd/yyyy")}
+            </time>
+          </span>
+          <span>
+            Last updated •{" "}
+            <time dateTime={updatedAt.toISOString()}>
+              {format(updatedAt, "MM/dd/yyyy")}
+            </time>
+          </span>
         </div>
       </div>
       {children}
