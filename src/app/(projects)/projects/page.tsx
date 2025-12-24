90a22fbb455803a5315b9e636eb3fcfc3fca5d93
axxo1337 <58981352+axxo1337@users.noreply.github.com>
2025-12-24 13:59:24 -0500

    feat: Projects page

--- src/app/(projects)/projects/page.tsx
+++ src/app/(projects)/projects/page.tsx
@@ -1,20 +1,49 @@
 import Main from "@/components/layout/Main";
+import Section from "@/components/layout/Section";
+import Project from "@/components/page/Projects/Project";
+import { getAllContentMetadata } from "@/lib/server/mdx";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Projects",
 };
 
-export default function Projects() {
+export default async function Projects() {
+  const projects = (await getAllContentMetadata("project")).sort((a, b) => {
+    return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+  });
+
+  const mostRecentUpdate = projects.reduce((latest, project) => {
+    const projectUpdate = new Date(project.updatedAt);
+    return projectUpdate > latest ? projectUpdate : latest;
+  }, new Date(0));
+
   return (
     <Main
       title="Projects"
       createdAt={new Date(2025, 11, 22)}
-      updatedAt={new Date(2025, 11, 22)}
+      updatedAt={mostRecentUpdate}
     >
-      <div className="my-12">
-        <span className="text-4xl font-semibold">Coming soon...</span>
-      </div>
+      <Section title="Library">
+        <p className="md:text-lg">
+          This page doesn&apos;t list ALL of my project, but you can find some
+          of my most recent here. Clicking any of them will direct you to
+          another page dedicated to describing said project.
+        </p>
+        <hr className="my-4 md:my-6 border-white/20" />
+        <div className="grid md:grid-cols-2 gap-6 md:gap-8">
+          {projects.map((project, projectIndex) => (
+            <Project
+              key={`project-${projectIndex}`}
+              title={project.title}
+              description={project.description as string}
+              createdAt={new Date(project.createdAt)}
+              thumbnailPath={project.thumbnailPath as string}
+              href={`/project/${project.slug}`}
+            />
+          ))}
+        </div>
+      </Section>
     </Main>
   );
 }
---
+++ src/components/page/Projects/Project.tsx
@@ -0,0 +1,44 @@
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { format } from "date-fns";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Project({
+  title,
+  description,
+  createdAt,
+  thumbnailPath,
+  href,
+}: ProjectProps) {
+  return (
+    <Link href={href} className="group">
+      <article>
+        <AspectRatio ratio={16 / 9}>
+          <Image
+            src={thumbnailPath}
+            fill={true}
+            alt="thumnail"
+            className="rounded-sm border-2 border-white/20"
+          />
+        </AspectRatio>
+        <div className="my-2">
+          <span className="text-2xl font-medium group-hover:underline">
+            {title}
+          </span>
+          <div className="flex flex-wrap gap-4 items-center text-sm font-medium text-white/75 mt-0.5">
+            <span>Created • {format(createdAt, "MM/dd/yyyy")}</span>
+          </div>
+        </div>
+        <p className="text-white/75">{description}</p>
+      </article>
+    </Link>
+  );
+}
+
+interface ProjectProps {
+  title: string;
+  description: string;
+  createdAt: Date;
+  thumbnailPath: string;
+  href: string;
+}
