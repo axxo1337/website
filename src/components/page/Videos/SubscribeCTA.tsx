b9cae968ad0b1c792b7a1e80aae3c8a4987c7278
axxo1337 <58981352+axxo1337@users.noreply.github.com>
2026-03-21 09:32:45 -0400

    fix: Padding on mobile subscribe CTA

--- src/components/page/Videos/SubscribeCTA.tsx
+++ src/components/page/Videos/SubscribeCTA.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 
 export default function SubscribeCTA() {
   return (
-    <div className="relative isolate mt-12 flex flex-col items-center gap-5 overflow-hidden rounded-2xl border border-white/10 px-4 py-10 text-center sm:gap-6 sm:px-7">
+    <div className="relative isolate mt-12 flex flex-col items-center gap-5 overflow-hidden rounded-2xl border border-white/10 px-3 py-7 text-center sm:gap-6 sm:px-7">
       <span className="text-2xl md:text-4xl font-semibold">Enjoying my work?</span>
       <p className="text-white/70 md:text-lg md:w-[70%]">
         Subscribe to my YouTube for high-quality videos and to support me!
