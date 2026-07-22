2bf7ae474fd703aed6674401de452e5b95b73982
axxo1337 <58981352+axxo1337@users.noreply.github.com>
2026-07-21 21:41:07 -0400

    feat: New texture

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -106,7 +106,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${roboto.variable} antialiased flex flex-col items-center overflow-x-hidden relative bg-[url('/images/texture.png')] bg-fixed bg-center bg-cover`}
+        className={`${roboto.variable} antialiased flex flex-col items-center overflow-x-hidden relative bg-[url('/images/bliss.png')] bg-fixed bg-center bg-cover`}
       >
         <div className="px-2.5 md:px-5 lg:px-0 max-w-240 w-full">
           <Header />
