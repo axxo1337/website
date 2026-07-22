921d0870bb9df814cb60d33c989bff55b9205d52
axxo1337 <58981352+axxo1337@users.noreply.github.com>
2026-07-22 12:23:14 -0400

    feat: New colors

--- src/components/page/Home/Social.tsx
+++ src/components/page/Home/Social.tsx
@@ -8,8 +8,16 @@ export default function Social({ href, iconPath, title }: Social) {
         href={href}
         className="flex items-center group gap-2 border border-white/20 hover:border-white duration-250 transition-colors rounded-full py-2 px-4 blured-bg"
       >
-        <Image src={iconPath} className="size-6 md:size-8" width={32} height={32} alt={title} />
-        <span className="font-medium text-lg md:text-xl">{title}</span>
+        <Image
+          src={iconPath}
+          className="size-6 md:size-8 group-hover:brightness-85 duration-250 transition-all"
+          width={32}
+          height={32}
+          alt={title}
+        />
+        <span className="font-medium text-lg md:text-xl group-hover:text-white/80 transition-colors duration-250 group-hover-underline">
+          {title}
+        </span>
       </Link>
     </li>
   );
