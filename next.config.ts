4efb027c87e3bbee056f234123bc3221389af40d
aXXo-dev <0381129@csvdc.qc.ca>
2026-08-09 07:45:15 -0400

    fix: Restore headers in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,19 @@ const nextConfig: NextConfig = {
     minimumCacheTTL: 604800,
     formats: ["image/avif", "image/webp"],
   },
+  async headers() {
+    return [
+      {
+        source: "/images/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=15768000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 const withMDX = createMDX({
