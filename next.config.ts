c93300ef3e4d4325e03926141a675b757a0d7f8a
axxo1337 <58981352+axxo1337@users.noreply.github.com>
2026-08-09 08:24:59 -0400

    feat: Moved missing headers into config

--- next.config.ts
+++ next.config.ts
@@ -16,11 +16,37 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
+        source: "/:path*",
+        headers: [
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=()",
+          },
+        ],
+      },
+      {
         source: "/images/:path*",
         headers: [
           {
             key: "Cache-Control",
             value: "public, max-age=15768000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/_next/static/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
           },
         ],
       },
