f7c8f6909810588a7f7062d1b7ad526ef41124d6
aXXo-dev <0381129@csvdc.qc.ca>
2026-08-14 05:08:55 -0400

    feat: Created a 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <main className="h-[calc(100dvh-184px-56px)] sm:h-[calc(100dvh-140px-56px)] flex flex-col gap-8 items-center justify-center">
+      <h1 className="text-5xl md:text-7xl font-semibold">Not Found</h1>
+      <p className="text-center max-w-lg">
+        It doesn&apos;t seem like I could find what you&apos;re looking for... Perhaps the page was removed? Anyways, you can go
+        back home using{" "}
+        <Link href="/" className="inline-anchor">
+          this link
+        </Link>
+        .
+      </p>
+    </main>
+  );
+}
