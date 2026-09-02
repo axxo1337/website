55d5c6fa10f6699cee95b05b7ad6210f3f309712
axxo1337 <58981352+axxo1337@users.noreply.github.com>
2026-09-01 20:10:57 -0400

    feat: New banner and better texture

--- src/components/ui/TexturedContainer.tsx
+++ src/components/ui/TexturedContainer.tsx
@@ -2,9 +2,14 @@ import { cn } from "@/lib/client/utils";
 
 export default function TexturedContainer({ children, className }: TexturedContainerProps) {
   return (
-    <div className={cn("relative overflow-hidden rounded-md", className)}>
+    <div
+      className={cn(
+        "[--textured-container-rounded:var(--radius-md)] relative overflow-hidden rounded-(--textured-container-rounded)",
+        className,
+      )}
+    >
       {children}
-      <div className="pointer-events-none absolute inset-0 shadow-[inset_0_0_12.5px_rgba(255,255,255,0.30)]" />
+      <div className="rounded-[calc(var(--textured-container-rounded)-2px)] pointer-events-none absolute inset-0 shadow-[inset_0_0_12.5px_rgba(255,255,255,0.30)]" />
     </div>
   );
 }
