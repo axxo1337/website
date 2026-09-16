2b137d447b2cad038c4771d27202dd70d289127a
aXXo-dev <0381129@csvdc.qc.ca>
2026-09-16 12:39:19 -0400

    feat: Added callout feature

---
+++ src/components/ui/Callout.tsx
@@ -0,0 +1,27 @@
+import { Lightbulb, OctagonAlert, TriangleAlert } from "lucide-react";
+import TexturedContainer from "./TexturedContainer";
+import { cn } from "@/lib/client/utils";
+
+const calloutObjectMap: Record<string, { icon: React.ReactNode, iconColor: string, containerClassName: string | null}> = {
+  info: { Icon: Lightbulb, iconColor: "#FFF", containerClassName: null },
+  warning: { Icon: TriangleAlert, iconColor: "#FBDD31", containerClassName: "bg-[#FBDD31]/20" },
+  danger: { Icon: OctagonAlert, iconColor: "#FB3131", containerClassName: "bg-[#FB3131]/20" },
+};
+
+export default function Callout({ children, type = "info" }: CalloutProps) {
+  const { Icon, iconColor, containerClassName } = calloutObjectMap[type];
+
+  return (
+    <TexturedContainer className={cn("my-4 p-3.5 py-4.5 flex gap-2 items-start", containerClassName)}>
+      <Icon style={{ color: iconColor }} className="shrink-0" size={20} />
+      <p className="leading-4.5">
+        {children}
+      </p>
+    </TexturedContainer>
+  );
+}
+
+interface CalloutProps {
+  children: React.ReactNode;
+  type?: "info" | "warning" | "danger";
+}
